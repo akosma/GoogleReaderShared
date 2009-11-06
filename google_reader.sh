20d47e30e3fd3fe018b9eb71643fb5cf2a2a2b1d
Adrian Kosmaczewski <akosma@gmail.com>
2009-11-06 12:05:54 +0100

    Modified parameters

--- google_reader.sh
+++ google_reader.sh
@@ -4,13 +4,16 @@
 # You can find yours by opening your own Google Shared items page.
 
 USER="00017030023266340908"
-QUANTITY=100000
+QUANTITY=5000
 FILENAME="google_reader.xml"
 
 echo Downloading the RSS XML feed with Google Reader shared items
 curl http://www.google.com/reader/public/atom/user%2F${USER}%2Fstate%2Fcom.google%2Fbroadcast\?r=n\&n=${QUANTITY} > ${FILENAME}
 
-for i in 2006 2007 2008 2009; do
+echo Generating one HTML file with all the titles of the shared articles
+ruby parse.rb ${FILENAME} > shared_all.html
+
+for i in 2005 2006 2007 2008 2009; do
     echo Generating HTML files with the titles of the shared articles in ${i}
-    ruby parse.rb ${FILENAME} ${i} > ${i}.html
+    ruby parse.rb ${FILENAME} ${i} > shared_${i}.html
 done
